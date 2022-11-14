a40002ba9eede92ab29c904d605bcfe49d7f06dc
chirag127 <whyiswhen@gmail.com>
2022-11-14 17:07:13 +0000

    cushman

--- codegeex.py
+++ codegeex.py
@@ -34,7 +34,7 @@ problems = read_problems()
 num_samples_per_task = 200
 samples = [
     dict(
-        task_id=task_id, completion=generate_one_completion(problems[task_id]["prompt"])
+        task_id=task_id, completion=generate(problems[task_id]["prompt"])
     )
     for task_id in problems
     for _ in range(num_samples_per_task)
---
+++ cushman.py
@@ -0,0 +1,38 @@
+import os
+import openai
+from apikey import openai_key
+
+openai.api_key = openai_key
+
+def generate(p):
+
+    response = openai.Completion.create(
+  model="code-cushman-001",
+  prompt=p,
+  temperature=0.2,
+  max_tokens=256,
+  top_p=1,
+  frequency_penalty=0,
+  presence_penalty=0,
+  stop=["\nclass", "\ndef"]
+)
+    code = response.choices[0].text
+    return code
+
+
+
+from human_eval.data import write_jsonl, read_problems
+
+problems = read_problems()
+
+num_samples_per_task = 200
+samples = [
+    dict(
+        task_id=task_id, completion=g(problems[task_id]["prompt"])
+    )
+    for task_id in problems
+    for _ in range(num_samples_per_task)
+]
+write_jsonl("samples.jsonl", samples)
+
+# print(generate("# Write a function that takes a string and returns a string")  )
