112be4b004fbd7fcbe81f330106c227ad4d233f1
Chirag <76880977+chirag127@users.noreply.github.com>
2022-11-15 07:34:20 +0000

    remove errror

--- cushman.py
+++ cushman.py
@@ -1,50 +1,55 @@
-import os
-import openai
+"""
+This module contains the code for the code generation model codex
+"""
+
 from time import sleep
-from apikey import openai_key
 
-openai.api_key = openai_key
+import openai
 
-def generate(p):
+from apikey import OPENAI_KEY
+from human_eval.data import read_problems, write_jsonl
+
+openai.api_key = OPENAI_KEY
+
+
+def generate(prompt):
+    """
+    The generate function takes a prompt as input and returns the generated code.
+    The prompt is a string of text that will be used to generate code. The function
+    returns the generated code as a string.
+    """
 
     try:
         sleep(3)
 
         response = openai.Completion.create(
-    # model="code-cushman-001",
-    model="code-davinci-002",
-    prompt=p,
-    temperature=0.2,
-    max_tokens=256,
-    top_p=1,
-    frequency_penalty=0,
-    presence_penalty=0,
-    stop=["\nclass", "\ndef"]
-    )
+            # model="code-cushman-001",
+            model="code-davinci-002",
+            prompt=prompt,
+            temperature=0.2,
+            max_tokens=256,
+            top_p=1,
+            frequency_penalty=0,
+            presence_penalty=0,
+            stop=["\nclass", "\ndef"],
+        )
         sleep(20)
         code = response.choices[0].text
-        print("".join([p, code]))
+        print("".join([prompt, code]))
         return code
 
-    except Exception as error:
+    except Exception as error:  # pylint: disable=broad-except
         print(error)
         print("Error")
         return ""
 
 
-
-from human_eval.data import write_jsonl, read_problems
-
 problems = read_problems()
 
-num_samples_per_task = 1
+NUM_SAMPLES_PER_TASK = 1
 samples = [
-    dict(
-        task_id=task_id, completion=generate(problems[task_id]["prompt"])
-    )
+    dict(task_id=task_id, completion=generate(problems[task_id]["prompt"]))
     for task_id in problems
-    for _ in range(num_samples_per_task)
+    for _ in range(NUM_SAMPLES_PER_TASK)
 ]
 write_jsonl("cd.jsonl", samples)
-
-# print(generate("# Write a function that takes a string and returns a string")  )
--- main.py
+++ main.py
@@ -1,29 +1,45 @@
-from apikey import cohere_key
+"""
+This module contains the functions for generating code completions in cohere
+"""
 
 import cohere
 
+from apikey import COHERE_KEY
+from human_eval.data import read_problems, write_jsonl
+
 
 def generate_one_completion(prompt):
+    """# This function generates one completion for a given prompt.
+    # It takes the prompt as input, and returns the completion as output.
+    # The prompt is a string.
+    # The completion is a string.
+    # The prompt is a single word.
+    # The completion is a single word.
+    # The prompt is a prefix of the completion.
+    # The prompt is a single word.
+    # The completion is a single word.
+    # The prompt is a prefix of the completion.
+    """
+
     try:
-        co = cohere.Client(cohere_key)
-        response = co.generate(
-        model="medium",
-        prompt=prompt,
-        max_tokens=100,
-        temperature=0,
-        k=0,
-        p=1,
-        frequency_penalty=0,
-        presence_penalty=0,
-        stop_sequences=[],
-        return_likelihoods="NONE",
-    )
+        cohere_client = cohere.Client(COHERE_KEY)
+        response = cohere_client.generate(
+            model="medium",
+            prompt=prompt,
+            max_tokens=100,
+            temperature=0,
+            k=0,
+            p=1,
+            frequency_penalty=0,
+            presence_penalty=0,
+            stop_sequences=[],
+            return_likelihoods="NONE",
+        )
         return response.generations[0].text
-    except Exception as e:
-        print(e)
+    except Exception as error:  # pylint: disable=broad-except
+        print(error)
         return "None"
 
-from human_eval.data import write_jsonl, read_problems
 
 problems = read_problems()
 
